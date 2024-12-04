9fa0f18f054919f744855de737f0fba9f60e7235
vvnfrd <vvnfrd@proton.me>
2024-12-04 19:24:46 +0700

    models are done

---
+++ main/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Table(models.Model):
+    """Модель стола"""
+
+    free = models.BooleanField(default=True, verbose_name='свободный')
+    seats = models.PositiveIntegerField(default=4, verbose_name='кол-во мест')
+    is_vip = models.BooleanField(default=False, verbose_name='вип статус')
+
+
+    def __str__(self):
+        return f'стол {self.pk} {"свободный" if self.free else "занятый"}'
+
+    class Meta:
+        verbose_name = 'стол'
+        verbose_name_plural = 'столы'
+
+class Booking(models.Model):
+    """Модель бронирования"""
+
+    table = models.ForeignKey("Table", on_delete=models.CASCADE,
+                              verbose_name='стол', **NULLABLE)
+    time_from = models.DateTimeField()
+    time_to = models.DateTimeField()
+    client_email = models.EmailField(unique=True, verbose_name='почта клиента')
+    client_name = models.CharField(max_length=50, verbose_name='имя клиента')
+    client_phone = models.CharField(max_length=11, verbose_name='телефон клиента')
+
+    def __str__(self):
+        return f'бронь стола {self.table} с {self.time_from} до {self.time_to}'
+
+    class Meta:
+        verbose_name = 'бронь'
+        verbose_name_plural = 'бронь'
---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    first_name = models.CharField(max_length=35, verbose_name='имя')
+    last_name = models.CharField(max_length=50, verbose_name='фамилия')
+    job_title = models.CharField(max_length=100, verbose_name='должность')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'работник'
+        verbose_name_plural = 'работники'
