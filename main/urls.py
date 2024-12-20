ad26d461513b6806912d02e55784f37747277ead
vvnfrd <vvnfrd@proton.me>
2024-12-20 16:26:56 +0700

    some edited

--- main/urls.py
+++ main/urls.py
@@ -15,5 +15,4 @@ urlpatterns = [
     path('table/update/<int:pk>/', cache_page(60)(TableUpdateView.as_view()), name='update'),
     path('table/delete/<int:pk>/', cache_page(60)(TableDeleteView.as_view()), name='delete'),
     path('booking/create/', cache_page(60)(BookingCreateView.as_view()), name='booking_create'),
-    path('booking/create/', cache_page(60)(TableListView.as_view()), name='table_list')
 ]
