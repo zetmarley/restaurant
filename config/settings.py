23e52cb1efa792a6f14b8795d3d923d1f2abe204
vvnfrd <vvnfrd@proton.me>
2025-01-17 00:21:34 +0700

    docker-compose finished

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'restaurant',
+        'NAME': os.getenv('DB_NAME', 'restaurant'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'PORT': 5432,
