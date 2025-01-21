3790d6fd4da70240568f299f44532133c7008670
vvnfrd <vvnfrd@proton.me>
2025-01-22 00:51:39 +0700

    updated booking validation

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,9 @@
+import datetime
+
 from django import forms
 from django.forms import HiddenInput
 
+from config.settings import local_tz
 from main.models import Booking, Table, Content
 
 
@@ -16,12 +19,26 @@ class BookingForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = super().clean()
+        client_email = cleaned_data.get('client_email')
         table = cleaned_data.get('table')
         time_from = cleaned_data.get('time_from')
         time_to = cleaned_data.get('time_to')
 
+        if len(Booking.objects.filter(client_email=client_email)) > 2:
+            raise forms.ValidationError("У вас уже есть 2 брони.")
+
         if time_from >= time_to:
             raise forms.ValidationError("Время начала бронирования должно быть раньше времени окончания.")
+
+        if time_from <= datetime.datetime.now(tz=local_tz):
+            raise forms.ValidationError("К сожалению, нельзя забронировать стол в прошлом времени.")
+
+        if time_from > datetime.datetime.now(tz=local_tz) + datetime.timedelta(days=15):
+            raise forms.ValidationError("Выберите время пораньше, пожалуйста.")
+
+        if time_to - time_from > datetime.timedelta(hours=6):
+            raise forms.ValidationError("К сожалению, нельзя забронировать стол на такой"
+                                        "долгий промежуток времени.")
 
         overlapping_bookings = Booking.objects.filter(
             table=table,
